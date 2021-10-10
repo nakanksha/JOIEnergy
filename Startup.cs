e20036500671cd9bedc903821f4114599b91340e
nakanksha <92204803+nakanksha@users.noreply.github.com>
2021-10-11 05:28:52 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JOIEnergy.Domain;
+using JOIEnergy.Enums;
+using JOIEnergy.Generator;
+using JOIEnergy.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace JOIEnergy
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var readings =
+                GenerateMeterElectricityReadings();
+
+            var pricePlans = new List<PricePlan> {
+                new PricePlan{
+                    EnergySupplier = Enums.Supplier.DrEvilsDarkEnergy,
+                    UnitRate = 10m,
+                    PeakTimeMultiplier = new List<PeakTimeMultiplier>()
+                },
+                new PricePlan{
+                    EnergySupplier = Enums.Supplier.TheGreenEco,
+                    UnitRate = 2m,
+                    PeakTimeMultiplier = new List<PeakTimeMultiplier>()
+                },
+                new PricePlan{
+                    EnergySupplier = Enums.Supplier.PowerForEveryone,
+                    UnitRate = 1m,
+                    PeakTimeMultiplier = new List<PeakTimeMultiplier>()
+                }
+            };
+
+            //services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddTransient<IAccountService, AccountService>();
+            services.AddTransient<IMeterReadingService, MeterReadingService>();
+            services.AddTransient<IPricePlanService, PricePlanService>();
+            services.AddSingleton((IServiceProvider arg) => readings);
+            services.AddSingleton((IServiceProvider arg) => pricePlans);
+            services.AddSingleton((IServiceProvider arg) => SmartMeterToPricePlanAccounts);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)//IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+            //app.UseRouting();
+            
+
+            /*
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                   name: "default",
+                   pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapDefaultControllerRoute();
+                endpoints.MapRazorPages();
+            });
+            */
+            //app.UseMvc();
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World");
+            //});
+            //app.UseStaticFiles();
+            //app.UseRouting();
+            //app.UseCors();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+              //  endpoints.MapControllers();
+            //});
+        }
+
+        private Dictionary<string, List<ElectricityReading>> GenerateMeterElectricityReadings() {
+            var readings = new Dictionary<string, List<ElectricityReading>>();
+            var generator = new ElectricityReadingGenerator();
+            var smartMeterIds = SmartMeterToPricePlanAccounts.Select(mtpp => mtpp.Key);
+
+            foreach (var smartMeterId in smartMeterIds)
+            {
+                readings.Add(smartMeterId, generator.Generate(20));
+            }
+            return readings;
+        }
+
+        public Dictionary<String, Supplier> SmartMeterToPricePlanAccounts
+        {
+            get
+            {
+                Dictionary<String, Supplier> smartMeterToPricePlanAccounts = new Dictionary<string, Supplier>();
+                smartMeterToPricePlanAccounts.Add("smart-meter-0", Supplier.DrEvilsDarkEnergy);
+                smartMeterToPricePlanAccounts.Add("smart-meter-1", Supplier.TheGreenEco);
+                smartMeterToPricePlanAccounts.Add("smart-meter-2", Supplier.DrEvilsDarkEnergy);
+                smartMeterToPricePlanAccounts.Add("smart-meter-3", Supplier.PowerForEveryone);
+                smartMeterToPricePlanAccounts.Add("smart-meter-4", Supplier.TheGreenEco);
+                return smartMeterToPricePlanAccounts;
+            }
+        }
+    }
+}
